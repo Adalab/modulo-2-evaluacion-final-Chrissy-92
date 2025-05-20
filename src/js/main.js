ea91f7772b540ce2e0615f351e4272fd846fb026
Cristina Pérez <cristinaperezhuerta@gmal.com>
2025-05-20 11:57:51 +0200

    Defining handler functions for select click events, adding classes, and adding to favorites list

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const animelistUl = document.querySelector(".js_animelistUl");
+const animelistUl = document.querySelector(".js-animelistUl");
+const animelistfavoritesUl = document.querySelector(".js-animelistfavoritesUl");
 const textSearchInput = document.querySelector(".js-textSearchInput");
 const btnSearch = document.querySelector(".js-btnSearch");
 const btnReset = document.querySelector(".js-btnReset");
@@ -9,17 +10,22 @@ const btnReset = document.querySelector(".js-btnReset");
 
 let allAnimes = [];
 
+// const imageBroke = "";
+// if (imageBroke !== oneAnime.images.jpg.image_url) {
+//   imageBroke = `https://placehold.co/210x300/ffffff/555555?text=TV`;
+// }
+
 // FUNCIONES
 
 function renderOneAnime(oneAnime) {
   const html = `
-    <li class="anime__item" id=${oneAnime.id} >
-    <div class="anime__chromo">
-    <h2 class="title__anime-item">${oneAnime.title}</h2>
-    <img src=${oneAnime.images.jpg.image_url} />
-    </div>
+    <li class="anime__item js-animeLi" data-id=${oneAnime.mal_id}>
+      <div class="anime__chromo">
+        <h2 class="title__anime-item">${oneAnime.title}</h2>
+        <img src=${oneAnime.images.jpg.image_url} />
+      </div>
     </li>
-    `;
+  `;
 
   return html;
 }
@@ -32,6 +38,36 @@ function renderAllAnimes() {
   }
 
   animelistUl.innerHTML = html; // Cuando tenemos todos los li los ponemos en la página
+
+  const allAnimesLi = document.querySelectorAll(".js-animeLi");
+  for (const animeLi of allAnimesLi) {
+    animeLi.addEventListener("click", handleClickAnime);
+  }
+}
+
+function handleClickAnime(ev) {
+  // Detecta un elemento clicado por el usuario
+  const clickedLi = ev.currentTarget;
+  // AL elemento seleccionado, se le añade la clase favoriteAnime
+  clickedLi.classList.toggle("favoriteAnime");
+
+  // Obtiene el id del atributo seleccionado y se aplica parseInt para pasar de string a número
+  const anime_id = parseInt(clickedLi.dataset.id);
+  console.log(anime_id);
+
+  // Recorre el array de animes comparando los id, se queda con el que sea exactamente igual que el seleccionado por el usuario y lo devuelve
+  const clickedAnimeSelected = allAnimes.find(
+    (oneAnime) => oneAnime.mal_id === anime_id
+  );
+
+  console.log(clickedAnimeSelected);
+
+  // Generamos otro li para los animes seleccionados
+  const htmlOneAnime = renderOneAnime(clickedAnimeSelected);
+  console.log(htmlOneAnime);
+
+  // Añadimos los li seleccionados a la lista de favoritos
+  animelistfavoritesUl.innerHTML += htmlOneAnime;
 }
 
 // CUANDO CARGA LA PÁGINA
@@ -39,7 +75,7 @@ function renderAllAnimes() {
 fetch(`https://api.jikan.moe/v4/anime`)
   .then((res) => res.json())
   .then((data) => {
-    //console.log(data.data); El console nos sirve para ver qué elemento dentro de data contiene el array de elementos que nos interesa
+    //console.log(data.data); El console nos sirve para ver qué elemento dentro de data contiene el array de elementos que nos interesan
     allAnimes = data.data;
 
     renderAllAnimes();
