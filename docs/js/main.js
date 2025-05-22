2fcf4d2e57a793f4a2be906d043f575f6320964f
Cristina Pérez <cristinaperezhuerta@gmal.com>
2025-05-22 13:12:35 +0200

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,8 @@
-const u=document.querySelector(".js-formAnime"),f=document.querySelector(".js-animelistUl"),h=document.querySelector(".js-animelistfavoritesUl"),v=document.querySelector(".js-textSearchInput"),_=document.querySelector(".js-btnSearch"),S=document.querySelector(".js-btnReset");let l=[],i=[];function m(e){return i.findIndex(t=>t.mal_id===e.mal_id)===-1?`
+const f=document.querySelector(".js-formAnime"),u=document.querySelector(".js-animelistUl"),h=document.querySelector(".js-animelistfavoritesUl"),v=document.querySelector(".js-textSearchInput"),p=document.querySelector(".js-btnSearch"),_=document.querySelector(".js-btnReset");let a=[],n=[];function m(e){return n.findIndex(t=>t.mal_id===e.mal_id)===-1?`
       <li class="anime__item js-animeLi" data-id=${e.mal_id}>
         <div class="anime__chromo">
           <h2 class="title__anime-item">${e.title}</h2>
+          <p class="js-type">${e.type}</p>
           <img src=${e.images.jpg.image_url} />
         </div>
       </li>
@@ -12,8 +13,9 @@ const u=document.querySelector(".js-formAnime"),f=document.querySelector(".js-an
             <i class="fa-solid fa-square-xmark"></i>
           </span>
           <h2 class="title__anime-item">${e.title}</h2>
+          <p class="js-type">${e.type}</p>
           <img src=${e.images.jpg.image_url} />
         </div>
       </li>
-    `}function c(){let e="";for(const t of l)e+=m(t);f.innerHTML=e;const n=document.querySelectorAll(".js-animelistUl .js-animeLi");for(const t of n)t.addEventListener("click",g)}function a(){let e="";for(const t of i)e+=m(t);h.innerHTML=e;const n=document.querySelectorAll(".js-deleteIcon");for(const t of n)t.addEventListener("click",j)}function g(e){const n=e.currentTarget;n.classList.toggle("favoriteAnime");const t=parseInt(n.dataset.id),s=i.findIndex(o=>o.mal_id===t);if(s===-1){const o=l.find(d=>d.mal_id===t);i.push(o)}else i.splice(s,1);localStorage.setItem("favoritesAnimes",JSON.stringify(i)),a()}const j=e=>{console.log("Has hecho click en un icono de cancelar");const n=parseInt(e.currentTarget.id);console.log(n);const t=i.findIndex(s=>s.mal_id===n);console.log(t),i.splice(t,1),a(),c()};fetch("https://api.jikan.moe/v4/anime").then(e=>e.json()).then(e=>{l=e.data,c()});const r=JSON.parse(localStorage.getItem("favoritesAnimes"));r!==null&&(i=r,a());function p(e){e.preventDefault();const n=v.value;fetch(`https://api.jikan.moe/v4/anime?q=${n}`).then(t=>t.json()).then(t=>{l=t.data,c()})}function A(e){e.preventDefault(),u.reset(),i=[],localStorage.clear(),document.querySelectorAll(".favoriteAnime").forEach(n=>n.classList.remove("favoriteAnime")),a()}_.addEventListener("click",p);S.addEventListener("click",A);
+    `}function o(){let e="";for(const t of a)e+=m(t);u.innerHTML=e;const i=document.querySelectorAll(".js-animelistUl .js-animeLi");for(const t of i)t.addEventListener("click",S)}function l(){let e="";for(const t of n)e+=m(t);h.innerHTML=e;const i=document.querySelectorAll(".js-deleteIcon");for(const t of i)t.addEventListener("click",j)}function S(e){const i=e.currentTarget;i.classList.toggle("favoriteAnime");const t=parseInt(i.dataset.id),s=n.findIndex(c=>c.mal_id===t);if(s===-1){const c=a.find(d=>d.mal_id===t);n.push(c)}else n.splice(s,1);localStorage.setItem("favoritesAnimes",JSON.stringify(n)),l()}const j=e=>{const i=parseInt(e.currentTarget.id),t=n.findIndex(s=>s.mal_id===i);n.splice(t,1),l(),o()};fetch("https://api.jikan.moe/v4/anime").then(e=>e.json()).then(e=>{a=e.data,o()});const r=JSON.parse(localStorage.getItem("favoritesAnimes"));r!==null&&(n=r,l());function g(e){e.preventDefault();const i=v.value;fetch(`https://api.jikan.moe/v4/anime?q=${i}`).then(t=>t.json()).then(t=>{a=t.data,o()})}function y(e){e.preventDefault(),f.reset(),n=[],localStorage.clear(),document.querySelectorAll(".favoriteAnime").forEach(i=>i.classList.remove("favoriteAnime")),l()}p.addEventListener("click",g);_.addEventListener("click",y);
 //# sourceMappingURL=main.js.map
